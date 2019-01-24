57ab8ecfdd80dc856b28c29534dd5d22f7c5baba
Saheed Oladosu <oladosu777@gmail.com>
2019-01-24 18:05:06 +0100

    new add

--- sum.js
+++ sum.js
@@ -1,8 +1,8 @@
-function add(number){
+function add(...numbers){
 	
-	for(let i = 0; i < number.length; i++){
-		number += number[i];
+	for(let i = 0; i < numbers.length; i++){
+		numbers += numbers[i];
 	}
-	return number;
+	return numbers;
 }
 console.log(add([2,7,8,5,4]));
