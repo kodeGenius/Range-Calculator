9300c413e50b9b0b5e4b2194d12d9a05134fc249
Saheed Oladosu <oladosu777@gmail.com>
2019-01-27 14:04:29 +0100

    redesigned logic

--- sum.js
+++ sum.js
@@ -1,8 +1,8 @@
-function add(...numbers){
-	
+function add(numbers){
+	let output = 0;
 	for(let i = 0; i < numbers.length; i++){
-		numbers += numbers[i];
+		output += numbers[i];
 	}
-	return numbers;
+	return output;
 }
 console.log(add([2,7,8,5,4]));
